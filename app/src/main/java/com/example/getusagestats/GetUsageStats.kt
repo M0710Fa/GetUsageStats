35e1d02beade5f5c8eb8a51fe8d096f079133e9f
M0710Fa <b1018188.fun@gmail.com>
2022-06-02 11:18:52 +0900

    Initial commit

---
+++ app/src/main/java/com/example/getusagestats/GetUsageStats.kt
@@ -0,0 +1,9 @@
+package com.example.getusagestats
+
+import android.app.usage.UsageStatsManager
+import android.content.Context
+
+class GetUsageStats(val context: Context) {
+    private val TAG = "GetUsageStats"
+
+}
---
+++ app/src/main/java/com/example/getusagestats/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.getusagestats
+
+import android.app.AppOpsManager
+import android.content.ContentValues
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Process
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.getusagestats.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+        if (!checkForPermission()) {
+            Log.i(ContentValues.TAG, "The user may not allow the access to apps usage. ")
+            Toast.makeText(
+                this,
+                "Failed to retrieve app usage statistics. " +
+                        "You may need to enable access for this app through " +
+                        "Settings > Security > Apps with usage access",
+                Toast.LENGTH_LONG
+            ).show()
+            startActivity(Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS))
+        } else {
+            // We have the permission. Query app usage stats.
+            val filePath = filesDir.path + "/myText.txt"
+            Log.d("Test", "path : $filePath")
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun checkForPermission(): Boolean {
+        val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+        val mode = appOps.unsafeCheckOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, Process.myUid(), packageName)
+        return mode == AppOpsManager.MODE_ALLOWED
+    }
+}
