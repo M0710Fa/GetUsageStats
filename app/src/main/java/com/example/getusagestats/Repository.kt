1cfbe9559b1134139adbdea159beea7949a1469f
Miyata <b1018188.fun@gmail.com>
2022-06-02 14:13:39 +0900

    add saveFile and readFile function

--- app/src/main/java/com/example/getusagestats/Repository.kt
+++ app/src/main/java/com/example/getusagestats/Repository.kt
@@ -1,4 +1,30 @@
 package com.example.getusagestats
 
-class Repository {
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import java.io.BufferedReader
+import java.io.File
+
+class Repository() {
+    fun saveFile(fileName: String, str: String){
+        val appContext = GetUsageStatsApp().applicationContext
+        appContext.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+            it.write(str.toByteArray())
+        }
+    }
+
+
+    fun readFile(fileName: String): String?{
+        val appContext = GetUsageStatsApp().applicationContext
+        val readFile = File(appContext.filesDir, fileName)
+
+        if(!readFile.exists()){
+            Log.d("debug","No file exists")
+            return null
+        }
+        else{
+            return readFile.bufferedReader().use(BufferedReader::readText)
+        }
+    }
 }
